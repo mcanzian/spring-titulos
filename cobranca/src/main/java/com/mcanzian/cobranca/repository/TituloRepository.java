62daab26ffa8939e7471eb0b536c32ead49abc86
mcanzian <canzian.marcelo@hotmail.com>
2016-01-16 22:39:44 -0200

    Adição do repositório de Titulo

---
+++ cobranca/src/main/java/com/mcanzian/cobranca/repository/TituloRepository.java
@@ -0,0 +1,9 @@
+package com.mcanzian.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mcanzian.cobranca.model.Titulo;
+
+public interface TituloRepository extends JpaRepository<Titulo, Long> {
+
+}
