c4afce4d8cd7d4053a26df9807f706deb0489c25
mcanzian <canzian.marcelo@hotmail.com>
2016-01-16 22:44:56 -0200

    Implementação de mensagem de sucesso na view CadastroTitulo

--- cobranca/src/main/java/com/mcanzian/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/mcanzian/cobranca/controller/TituloController.java
@@ -1,14 +1,34 @@
 package com.mcanzian.cobranca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.mcanzian.cobranca.model.Titulo;
+import com.mcanzian.cobranca.repository.TituloRepository;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
+	
+	@Autowired
+	private TituloRepository titulos;
 
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo() {
 		return "CadastroTitulo";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView salvar(Titulo titulo) {
+		titulos.save(titulo);
+		
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagemSucesso", "Título salvo com sucessso.");
+		
+		return mv;
 	}
 	
 }
