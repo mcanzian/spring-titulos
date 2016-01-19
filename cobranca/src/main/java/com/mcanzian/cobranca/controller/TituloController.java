18729c8fac6ea848daf2d294caa8f4204b061a62
mcanzian <canzian.marcelo@hotmail.com>
2016-01-19 15:47:38 -0200

    Adição da função editar títulos

--- cobranca/src/main/java/com/mcanzian/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/mcanzian/cobranca/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,10 +24,11 @@ public class TituloController {
 	
 	@Autowired
 	private TituloRepository titulos;
+	private static String CADASTRO_VIEW = "CadastroTitulo";
 
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject("titulo", new Titulo());
 		return mv;
 	}
@@ -34,7 +36,7 @@ public class TituloController {
 	@RequestMapping(method = RequestMethod.POST)
 	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if (errors.hasErrors())
-			return "CadastroTitulo";
+			return CADASTRO_VIEW;
 		
 		titulos.save(titulo);
 		attributes.addFlashAttribute("mensagemSucesso", "Título salvo com sucessso.");
@@ -48,6 +50,15 @@ public class TituloController {
 		
 		List<Titulo> todosTitulos = titulos.findAll();
 		mv.addObject("titulos", todosTitulos);
+		
+		return mv;
+	}
+	
+	@RequestMapping("{id}")
+	public ModelAndView editar(@PathVariable("id") Titulo titulo) {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		
+		mv.addObject("titulo", titulo);
 		
 		return mv;
 	}
